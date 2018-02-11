9edfa410bcfa68b28b1106c1fef6a6a24a238ca8
aditya subramanyam <akatakamediga@hotels.com>
2018-02-11 16:53:17 +0530

    added eslint config

---
+++ index.js
@@ -0,0 +1,37 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'sourceType': 'module'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'no-unused-vars': [
+            'error',
+            {
+                'varsIgnorePattern': 'createVirtualNode|loki'
+            }
+        ]
+    }
+};
